d774b4c820417fb0f7da10d073ddc5c0eb50da20
Fabio Nascimento <agsuperpoder@gmail.com>
2019-12-11 11:35:28 -0300

    add dotenv

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express()
 
+require('dotenv').config()
+
 app.get('/', (req, res) => {
   res.send('hello 1')
 })
 
-app.listen(3000)
+const port = process.env.PORT
+app.listen(port, () => {
+  console.log(`Run server at: http://localhost:${port}/`)
+})
