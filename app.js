bafeb911a8f3297d42c5d1cc453a95b8fb5fc578
Fabio Nascimento <agsuperpoder@gmail.com>
2019-12-11 12:07:15 -0300

    add mongoose and config

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
 const app = express()
 
 require('dotenv').config()
+
+const DATABASE = process.env.DATABASE
+mongoose.connect(DATABASE, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useFindAndModify: true
+}).then(() => {
+  console.log('DATABASE connected!')
+})
 
 app.get('/', (req, res) => {
   res.send('hello 1')
