40227043521ad553e723637ec9565bdff3d777cd
Fabio Nascimento <agsuperpoder@gmail.com>
2023-03-14 00:09:34 -0300

    feat: add pagination

--- src/controllers/company.js
+++ src/controllers/company.js
@@ -1,68 +1,76 @@
-const Company = require('../models/company')
-const { errorHandler } = require('../helpers/dbErrorHandler')
+const Company = require("../models/company");
+const { errorHandler } = require("../helpers/dbErrorHandler");
 
 exports.create = (req, res) => {
   // console.log("req.body", req.body);
-  const company = new Company(req.body)
+  const company = new Company(req.body);
   company.save((err, company) => {
     if (err) {
       return res.status(400).json({
-        error: errorHandler(err)
-      })
+        error: errorHandler(err),
+      });
     }
     res.json({
-      company
-    })
-  })
-}
+      company,
+    });
+  });
+};
 
 exports.listByUser = (req, res) => {
-  const { offset, limit } = req.query
+  const { offset, limit } = req.query;
+  const page = offset ?? 1;
+  const pagination = limit ?? 2;
   Company.find()
     .where({
-      user: req.params.userId
+      user: req.params.userId,
     })
-    .limit(parseInt(limit ?? 10))
-    .skip((offset ?? 0 - 1) * limit ?? 2)
+    .limit(parseInt(pagination))
+    .skip(parseInt(page - 1) * pagination ?? 2)
     .exec(function (err, Companies) {
-      if (err) {
-        return res.status(400).json({
-          error: errorHandler(err)
-        })
-      } else {
-        res.status(200).json(Companies)
-      }
-    })
-}
+      Company.count().exec(function (err, count) {
+        if (err) {
+          return res.status(400).json({
+            error: errorHandler(err),
+          });
+        } else {
+          res.status(200).json({
+            content: Companies,
+            current: parseInt(page),
+            pages: Math.ceil(parseInt(count / pagination)),
+          });
+        }
+      });
+    });
+};
 
 exports.listActiveByUser = (req, res) => {
   Company.find()
     .where({
       user: req.params.userId,
-      active: 1
+      active: 1,
     })
     .exec(function (err, Companies) {
       if (err) {
         return res.status(400).json({
-          error: errorHandler(err)
-        })
+          error: errorHandler(err),
+        });
       } else {
-        res.status(200).json(Companies)
+        res.status(200).json(Companies);
       }
-    })
-}
+    });
+};
 
 exports.update = (req, res) => {
   Company.findById(req.params.id, function (err, company) {
-    if (err) res.send(err)
-    company.active = req.params.status
+    if (err) res.send(err);
+    company.active = req.params.status;
     company.save((err, data) => {
       if (err) {
         return res.status(400).json({
-          error: errorHandler(err)
-        })
+          error: errorHandler(err),
+        });
       }
-      res.json(data)
-    })
-  })
-}
+      res.json(data);
+    });
+  });
+};
